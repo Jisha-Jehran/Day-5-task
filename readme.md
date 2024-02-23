6301bd364d54aec5c0316c74b3da955dade8485a
Jisha-Jehran <jishajehran@gmail.com>
2024-02-23 15:41:59 +0530

    New commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# DAY 5 TASK  
+  
+
+ * This repository was created for submitting the task which was given at the end of Day 5 session on GUVI's Full Stack Development program.
+ * The task is to Create a resume in JSON format. I have submitted the task inside _biodata.js_ file. 
+ * For the given JSON iterate over all for loops (for, for in, for of, forEach) 
+  and the solution for this was included inside _script.js_file
+
+
+
+    
+    _Thanks for evaluating the task ; Awaiting for your valuable feedback_
+
+      
+      **Happy coding :)**
---
+++ script.js
@@ -0,0 +1,30 @@
+//Creating a resume data in JSON format
+
+let mybiodata = {
+    "Basic information":{
+    "Name" : "Jisha Jehran. M",
+    "Father's name" : "P.M.Musthaffa",
+    "Mobile" : 1234567890,
+    "Gender" : "Female",
+    "Date of Birth" : "25th November 2020",
+    "Marital status" : "Unmarried",
+    "Religion" : "Islam",
+    "Nationality" : "Indian",
+    "Languages known" :"Tamil,English,Malayalam",
+    },
+    "Qualification" :[{
+   "Highest Qualification" :"Master of Science(Chemistry)",
+   "Disciple":"Chemistry",
+   "Passout year":"2022",
+   "CGPA":"9.0",
+}
+] ,
+
+    "Address" :[{ 
+        "Street":"R.C.Street",
+       "Town":"Marthandam",
+       "Pincode":"629165",
+    }]
+}
+
+console.log(mybiodata)
